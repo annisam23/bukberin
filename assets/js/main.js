45d2894063203bfde7760a15360d0af87ce1bfee
annisam23 <annisamrahma23@gmail.com>
2023-12-19 19:33:17 +0700

    clean code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Rapid
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/rapid-multipurpose-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
